0d39c6b735c10b030e4be60dbc8e06cccfc344c3
Benjamin Laurent <blaurent@fu-c1-p1.s19.be>
2022-02-03 15:06:33 +0100

    ui

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -17,7 +17,7 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = (t_list *)malloc(sizeof(t_list));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 	new->content = content;
 	new->next = NULL;
