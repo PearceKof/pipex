b1ad5a7b37c84f7b44af88d353b02785dbc4e0ec
Benjamin <benjaminlaurentpro00@gmail.com>
2022-06-28 16:21:29 +0200

    clean funct 2

--- pipex.c
+++ pipex.c
@@ -12,56 +12,46 @@
 
 #include "pipex.h"
 
-void	ft_son(char **av, char **env, int *fd)
+void exec(char *av, char **env)
 {
 	char	**option;
 	char	*cmdpath;
+
+	option = ft_split(av, ' ');
+	cmdpath = ft_cmdpath(ft_getpaths(env), option[0]);
+	if (!cmdpath)
+	{
+		ft_freetab(option);
+	}
+		error();
+	execve(cmdpath, option, env);
+}
+
+void	ft_son(char **av, char **env, int *fd)
+{
 	int		infd;
 
 	infd = open(av[1], O_RDWR);
 	if (infd == -1)
 		error();
-	option = ft_split(av[2], ' ');
-	cmdpath = ft_cmdpath(paths, option[0]);
 	dup2(infd, STDIN_FILENO);
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[0]);
-	execve(cmdpath, option, env);
+	exec(av[2], env);
 }
 
 void	ft_daddy(char **av, char **env, int *fd)
 {
-	char	**option;
-	char	*cmdpath;
 	int		outfd;
 
 	outfd = open(av[4], O_WRONLY | O_CREAT | O_WRONLY | O_TRUNC);
 	if (outfd == -1)
 		error();
-	option = ft_split(av[3], ' ');
-	cmdpath = ft_cmdpath(paths, option[0]);
 	dup2(fd[0], STDIN_FILENO);
 	dup2(outfd, STDOUT_FILENO);
 	close(fd[1]);
-	execve(cmdpath, option, env);
+	exec(av[3], env);
 }
-
-// int	ft_clearfile(char **av, char **paths, char **env, int *fd)
-// {
-// 	char	**option;
-// 	char	*cmdpath;
-// 	int		outfd;
-
-// 	outfd = open(av[4], O_WRONLY);
-// 	option = ft_split("clear", ' ');
-// 	cmdpath = ft_cmdpath(paths, option[0]);
-// 	dup2(fd[0], STDIN_FILENO);
-// 	dup2(outfd, STDOUT_FILENO);
-// 	close(fd[0]);
-// 	close(fd[1]);
-// 	execve(cmdpath, option, env);
-// 	return (-1);
-// }
 
 int	main(int ac, char **av, char **env)
 {
@@ -80,7 +70,7 @@ int	main(int ac, char **av, char **env)
 		if (pid == 0)
 			ft_son(av, env, fd);
 		ft_freetab(paths);
-		waitpid(pid1, NULL, 0);
+		waitpid(pid, NULL, 0);
 		ft_daddy(av, env, fd);
 	}
 	else
